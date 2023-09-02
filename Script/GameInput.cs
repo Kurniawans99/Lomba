bf7124aec9fb514121fd0cd71450b4e6876feae8
Egi <egikurniawan2490@gmail.com>
2023-09-02 07:12:40 +0700

    Add Assets

---
+++ Script/GameInput.cs
@@ -0,0 +1,51 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class GameInput : MonoBehaviour
+{
+    public event EventHandler<OnRunEventArgs> OnRun;
+    public event EventHandler OnDash;
+    public class OnRunEventArgs : EventArgs
+    {
+        public bool isRunning;
+    }
+    private PlayerInputActions playerInputActions;
+    void Awake()
+    {
+        playerInputActions = new PlayerInputActions();
+        playerInputActions.Player.Enable();
+        playerInputActions.Player.Run.performed += PlayerRun_Performed;
+        playerInputActions.Player.Run.canceled += PlayerRun_Canceled;
+        playerInputActions.Player.Power.started += PlayerDash_Started;
+    }
+
+    private void PlayerDash_Started(InputAction.CallbackContext context)
+    {
+        OnDash?.Invoke(this, EventArgs.Empty);
+    }
+
+    private void PlayerRun_Performed(InputAction.CallbackContext context)
+    {
+        OnRun?.Invoke(this, new OnRunEventArgs
+        {
+            isRunning = context.performed
+        });
+    }
+    private void PlayerRun_Canceled(InputAction.CallbackContext context)
+    {
+        OnRun?.Invoke(this, new OnRunEventArgs
+        {
+            isRunning = context.performed
+        });
+    }
+
+    public Vector2 GetMovementVector()
+    {
+        Vector2 inputValue = Vector2.zero;
+        inputValue = playerInputActions.Player.Move.ReadValue<Vector2>();
+        return inputValue.normalized;
+    }
+}
